e8cd28c7252272b50d3d8214216f32eabd005c58
michelle-vel <michellevsep@gmail.com>
2024-12-05 15:43:53 -0500

    half a6

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -1,14 +1,18 @@
-import Database from "../Database/index.js";
+import model from "./model.js";
+// import Database from "../Database/index.js";
+
 export function deleteCourse(courseId) {
-  const { courses, enrollments } = Database;
-  Database.courses = courses.filter((course) => course._id !== courseId);
-  Database.enrollments = enrollments.filter(
-    (enrollment) => enrollment.course !== courseId
-  );
+  return model.deleteOne({ _id: courseId });
+  // const { courses, enrollments } = Database;
+  // Database.courses = courses.filter((course) => course._id !== courseId);
+  // Database.enrollments = enrollments.filter(
+  //   (enrollment) => enrollment.course !== courseId
+  // );
 }
 
 export function findAllCourses() {
-  return Database.courses;
+  // return Database.courses;
+  return model.find();
 }
 
 export function findCoursesForEnrolledUser(userId) {
@@ -23,9 +27,11 @@ export function findCoursesForEnrolledUser(userId) {
 }
 
 export function createCourse(course) {
-  const newCourse = { ...course, _id: Date.now().toString() };
-  Database.courses = [...Database.courses, newCourse];
-  return newCourse;
+  delete course._id;
+  return model.create(course); 
+  // const newCourse = { ...course, _id: Date.now().toString() };
+  // Database.courses = [...Database.courses, newCourse];
+  // return newCourse;
 }
 
 export function updateCourse(courseId, courseUpdates) {
