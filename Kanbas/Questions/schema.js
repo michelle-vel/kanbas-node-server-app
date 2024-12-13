0b3f76c873ea4f81b89f866ea128725ceb308ecd
michelle-vel <michellevsep@gmail.com>
2024-12-12 19:16:13 -0500

    working with points

--- Kanbas/Questions/schema.js
+++ Kanbas/Questions/schema.js
@@ -5,7 +5,7 @@ const questionSchema = new mongoose.Schema(
       points: Number,
       question: String,
       type: { type: String, enum: ["True/False", "Multiple Choice", "Long Answer"] },
-      correctAnswer: String, // Optional for non-MC/TF questions
+      correctChoice: String, // Optional for non-MC/TF questions
       choices: [String], // Optional, for MC questions
       quiz: { type: mongoose.Schema.Types.ObjectId, ref: "QuizModel" },
       description: String,
